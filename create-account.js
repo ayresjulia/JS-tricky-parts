287c399f625b0bf67a00cd798af3b52c81a7f719
Julia Ayres <j.bobrineva@gmail.com>
2021-05-02 11:41:25 -0400

    passed tests for create-account.js

--- create-account.js
+++ create-account.js
@@ -1,5 +1,31 @@
-function createAccount(pin, amount) {
-
+function createAccount (pin, amount = 0) {
+	return {
+		checkBalance (inputPin) {
+			return inputPin === pin ? `$${amount}` : "Invalid PIN.";
+		},
+		deposit (inputPin, depositAmt) {
+			return inputPin === pin
+				? `Succesfully deposited $${depositAmt}. Current balance: $${(amount += depositAmt)}.`
+				: "Invalid PIN.";
+		},
+		withdraw (inputPin, withdrawalAmt) {
+			if (amount < withdrawalAmt) {
+				return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+			} else if (amount >= withdrawalAmt && inputPin === pin) {
+				return `Succesfully withdrew $${withdrawalAmt}. Current balance: $${(amount -= withdrawalAmt)}.`;
+			} else {
+				return "Invalid PIN.";
+			}
+		},
+		changePin (oldPin, newPin) {
+			if (oldPin === pin) {
+				pin = newPin;
+				return "PIN successfully changed!";
+			} else {
+				return "Invalid PIN.";
+			}
+		}
+	};
 }
 
 module.exports = { createAccount };
